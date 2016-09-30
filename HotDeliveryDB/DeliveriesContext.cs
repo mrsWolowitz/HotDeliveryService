16d6021465fae3d2c2f98d014c7be9f21de6b566
mrsWolowitz <young-fly@yandex.ru>
2016-09-30 16:05:56 +0300

    Добавила классы для работы с sqlite-хранилищем БД

---
+++ HotDeliveryDB/DeliveriesContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotDeliveryDB
+{
+    public class DeliveriesContext : DbContext
+    {
+        public DbSet<Delivery> Deliveries { get; set; }
+        private string ConnectionString { get; set; }
+
+        public DeliveriesContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Delivery>().HasKey(m => m.Id);
+            builder.Entity<Delivery>()
+                .Property(b => b.Status)
+                .ForSqliteHasDefaultValue("Available");
+            builder.Entity<Delivery>()
+                .Property(b => b.CreationTime)
+                .ForSqliteHasDefaultValueSql("DEFAULT DATETIME(CURRENT_TIMESTAMP, 'LOCALTIME')");
+            builder.Entity<Delivery>()
+                .Property(b => b.ModificationTime)
+                .ForSqliteHasDefaultValueSql("DEFAULT DATETIME(CURRENT_TIMESTAMP, 'LOCALTIME')");
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ConnectionString };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+
+            optionsBuilder.UseSqlite(connection);
+        }
+    }
+}
---
+++ HotDeliveryDB/SQLiteRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotDeliveryDB
+{
+    public class SQLiteRepository : IRepository
+    {
+        private string _ConnectionString { get; }
+        private DeliveriesContext Context { get; }
+
+
+        public SQLiteRepository(string connectionString)
+        {
+            _ConnectionString = connectionString;
+            using (Context = new DeliveriesContext(connectionString))
+            {
+                Context.Database.EnsureCreated();
+            }
+        }
+
+
+        public List<Delivery> GetDeliveryList()
+        {
+            using (DeliveriesContext context = new DeliveriesContext(_ConnectionString))
+            {
+                var subset = context.Deliveries;
+                List<Delivery> deliveries = context.Deliveries.ToList();
+                return deliveries;
+            }
+
+        }
+
+        public Delivery GetDelivery(int id)
+        {
+
+            var item = (from delivery in Context.Deliveries
+                        where delivery.Id == id
+                        select new Delivery
+                        {
+                            Status = delivery.Status,
+                            Title = delivery.Title,
+                            UserId = delivery.UserId,
+                            CreationTime = delivery.CreationTime,
+                            ModificationTime = delivery.ModificationTime,
+                            ExpirationTime = delivery.ExpirationTime
+                        }).FirstOrDefault();
+            return item;
+
+        }
+
+        public void Create(Delivery item)
+        {
+            using (DeliveriesContext context = new DeliveriesContext(_ConnectionString))
+            {
+                context.Deliveries.Add(item);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Delivery item)
+        {
+
+            using (DeliveriesContext context = new DeliveriesContext(_ConnectionString))
+            {
+                context.Deliveries.Update(item);
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
