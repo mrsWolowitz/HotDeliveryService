fef7e0a764b7c10e8c47c40b8ec7b469fee9798e
mrsWolowitz <mrsWolowitz@users.noreply.github.com>
2016-10-03 08:01:43 +0300

    Добавлен клиент для теста запуска Scheduler

---
+++ TestWindow/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestWindow
+{
+    partial class ClientTasks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartCreateDeliveriesBtn = new System.Windows.Forms.Button();
+            this.StopCreateDeliveryBtn = new System.Windows.Forms.Button();
+            this.ExpireDeliveriesBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartCreateDeliveriesBtn
+            // 
+            this.StartCreateDeliveriesBtn.Location = new System.Drawing.Point(1, 12);
+            this.StartCreateDeliveriesBtn.Name = "StartCreateDeliveriesBtn";
+            this.StartCreateDeliveriesBtn.Size = new System.Drawing.Size(127, 23);
+            this.StartCreateDeliveriesBtn.TabIndex = 0;
+            this.StartCreateDeliveriesBtn.Text = "Start create deliveries";
+            this.StartCreateDeliveriesBtn.UseVisualStyleBackColor = true;
+            this.StartCreateDeliveriesBtn.Click += new System.EventHandler(this.StartCreateDeliveriesBtn_Click);
+            // 
+            // StopCreateDeliveryBtn
+            // 
+            this.StopCreateDeliveryBtn.Location = new System.Drawing.Point(1, 70);
+            this.StopCreateDeliveryBtn.Name = "StopCreateDeliveryBtn";
+            this.StopCreateDeliveryBtn.Size = new System.Drawing.Size(127, 23);
+            this.StopCreateDeliveryBtn.TabIndex = 1;
+            this.StopCreateDeliveryBtn.Text = "Stop all tasks";
+            this.StopCreateDeliveryBtn.UseVisualStyleBackColor = true;
+            this.StopCreateDeliveryBtn.Click += new System.EventHandler(this.StopTasksBtn_Click);
+            // 
+            // ExpireDeliveriesBtn
+            // 
+            this.ExpireDeliveriesBtn.Location = new System.Drawing.Point(1, 41);
+            this.ExpireDeliveriesBtn.Name = "ExpireDeliveriesBtn";
+            this.ExpireDeliveriesBtn.Size = new System.Drawing.Size(127, 23);
+            this.ExpireDeliveriesBtn.TabIndex = 2;
+            this.ExpireDeliveriesBtn.Text = "Expire deliveries";
+            this.ExpireDeliveriesBtn.UseVisualStyleBackColor = true;
+            this.ExpireDeliveriesBtn.Click += new System.EventHandler(this.ExpireDeliveries_Click);
+            // 
+            // ClientTasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 199);
+            this.Controls.Add(this.ExpireDeliveriesBtn);
+            this.Controls.Add(this.StopCreateDeliveryBtn);
+            this.Controls.Add(this.StartCreateDeliveriesBtn);
+            this.Name = "ClientTasks";
+            this.Text = "ClientTasks";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartCreateDeliveriesBtn;
+        private System.Windows.Forms.Button StopCreateDeliveryBtn;
+        private System.Windows.Forms.Button ExpireDeliveriesBtn;
+    }
+}
+
---
+++ TestWindow/Form1.cs
@@ -0,0 +1,64 @@
+﻿using SchedulerTasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWindow
+{
+
+    public partial class ClientTasks : Form
+    {
+        CancellationTokenSource _Cts1;
+        CancellationTokenSource _Cts2;
+        private Scheduler Scheduler { get; set; }
+        public ClientTasks()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Scheduler = new Scheduler();
+            if (Scheduler == null)
+                return;
+        }
+
+        private async void StartCreateDeliveriesBtn_Click(object sender, EventArgs e)
+        {
+            if (_Cts1 != null)
+                return;
+            using (_Cts1 = new CancellationTokenSource())
+            {
+                await Scheduler.CreateDeliveries(_Cts1.Token);
+            }
+            _Cts1 = null;
+
+        }
+
+        private void StopTasksBtn_Click(object sender, EventArgs e)
+        {
+            _Cts1?.Cancel();
+            _Cts2?.Cancel();
+        }
+
+        private async void ExpireDeliveries_Click(object sender, EventArgs e)
+        {
+            if (_Cts2 != null)
+                return;
+            using (_Cts2 = new CancellationTokenSource())
+            {
+
+                await Scheduler.ExpireDeliveries(_Cts2.Token);
+            }
+            _Cts2 = null;
+        }
+    }
+}
+
